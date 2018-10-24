5851cbc962b87e3b847fa970aa6829255f2bb8ae
Kirill <kireku@gmail.com>
2018-10-24 15:43:45 +0300

    Добавлены заглушки контроллеров

--- real_events_project/real_events_app/views.py
+++ real_events_project/real_events_app/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return render(request, 'real_events/index.html')
+
+def all_news(request):
+    return HttpResponse("all_news")
+def news_post(request, news_post_slug):
+    return HttpResponse("news_post")
+
+def all_places(request):
+    return HttpResponse("all_places")
+def place(request, place_slug):
+    return HttpResponse("place")
+
+def all_meetings(request):
+    return HttpResponse("all_meetings")
+def meeting(request, meeting_slug):
+    return HttpResponse("meeting")
+
+def all_referees(request):
+    return HttpResponse("all_referees")
+def referee(request, referee_slug):
+    return HttpResponse("referee")
+
+def all_teams(request):
+    return HttpResponse("all_teams")
+def team(request, team_slug):
+    return HttpResponse("team")
+
+def all_members(request):
+    return HttpResponse("all_members")
+def member(request, member_slug):
+    return HttpResponse("member")
+
+def all_staff(request):
+    return HttpResponse("all_staff")
+def staff(request, place_slug):
+    return HttpResponse("staff")
+
+
--- real_events_project/real_events_project/urls.py
+++ real_events_project/real_events_project/urls.py
@@ -16,9 +16,27 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-from django.urls import path, include
+from django.urls import path, re_path, include
+
+from real_events_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls'))
+    path('summernote/', include('django_summernote.urls')),
+
+    path('', views.index, name='index'),
+    path('places/', views.all_places, name='all_places'),
+    path('news/', views.all_news, name='all_news'),
+    path('meetings/', views.all_meetings, name='all_meetings'),
+    path('referees/', views.all_referees, name='all_referees'),
+    path('staff/', views.all_staff, name='all_staff'),
+    path('members/', views.all_members, name='all_members'),
+    path('teams/', views.all_teams, name='all_teams'),
+    re_path(r'^places/(?P<place_slug>[\w\-]+)/$', views.place, name='place'),
+    re_path(r'^news/(?P<news_post_slug>[\w\-]+)/$', views.news_post, name='news_post'),
+    re_path(r'^meetings/(?P<meeting_slug>[\w\-]+)/$', views.meeting, name='meeting'),
+    re_path(r'^referees/(?P<referee_slug>[\w\-]+)/$', views.referee, name='referee'),
+    re_path(r'^staff/(?P<staff_slug>[\w\-]+)/$', views.staff, name='staff'),
+    re_path(r'^members/(?P<member_slug>[\w\-]+)/$', views.member, name='member'),
+    re_path(r'^teams/(?P<team_slug>[\w\-]+)/$', views.team, name='team'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
